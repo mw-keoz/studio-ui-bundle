799345638e6b58a913426346b77f1e13be51aefa
Martin Wiesner <martin.wiesner@keoz.com>
2026-06-24 06:57:36 +0200

    build(studio): rebuild compiled MF remote for validation-list fix

--- public/build/c0ce6688-10d6-4f0c-b26e-a22cfdb53dd5/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/c0ce6688-10d6-4f0c-b26e-a22cfdb53dd5/static/js/remoteEntry.js'
-    
---
+++ public/build/cd1a22eb-9772-4f91-bc85-e20d271ee674/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/cd1a22eb-9772-4f91-bc85-e20d271ee674/static/js/remoteEntry.js'
+    
