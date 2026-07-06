689b789b13bfb425454f3a130d4c92673d52d668
Martin Wiesner <martin.wiesner@keoz.com>
2026-07-06 14:31:04 +0200

    build(studio): rebuild MF remote for 2026.1.6
    Regenerate public/build (module-federation SDK + app) on top of upstream
    v2026.1.6 carrying the three source patches:
    - SDK export of useFormGroupOptional / FormGroupContext
    - element validation errors rendered as a list
    - searchable relation columns + opt-in grid sorting

---
+++ public/build/93083711-0d14-4d8b-98e3-0d68a2305b9d/static/js/async/1207.f1851fd1.js
@@ -0,0 +1,2 @@
+/*! For license information please see 1207.f1851fd1.js.LICENSE.txt */
+"use strict";(self["chunk_pimcore_studio_ui_bundle "]=self["chunk_pimcore_studio_ui_bundle "]||[]).push([["1207"],{5338(e,o,t){var u=t(8090);o.createRoot=u.createRoot,o.hydrateRoot=u.hydrateRoot}}]);
