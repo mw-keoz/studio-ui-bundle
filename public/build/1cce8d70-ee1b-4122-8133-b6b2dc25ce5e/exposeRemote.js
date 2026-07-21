8408e5d81a931b2d1667593bfe3af6f235579058
Martin Wiesner <martin.wiesner@keoz.com>
2026-07-21 03:57:22 +0200

    build(studio): rebuild MF remote after reverting relation patch

---
+++ public/build/1cce8d70-ee1b-4122-8133-b6b2dc25ce5e/exposeRemote.js
@@ -0,0 +1,3 @@
+
+      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/1cce8d70-ee1b-4122-8133-b6b2dc25ce5e/static/js/remoteEntry.js'
+    
--- public/build/93083711-0d14-4d8b-98e3-0d68a2305b9d/exposeRemote.js
+++
@@ -1,3 +0,0 @@
-
-      window.StudioUIBundleRemoteUrl = '/bundles/pimcorestudioui/build/93083711-0d14-4d8b-98e3-0d68a2305b9d/static/js/remoteEntry.js'
-    
